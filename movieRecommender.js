3bd97a57128960dc68b45ddf8abfbab15f767ff1
Daniel Baek <144073959+danielbaek2@users.noreply.github.com>
2023-09-22 09:32:24 -0400

    Update movieRecommender.js
    Added constants for API search usage

--- movieRecommender.js
+++ movieRecommender.js
@@ -1,4 +1,9 @@
 //TMDB
 
-const API_KEY = '804511dd3d7ef6d6dcdd03d0d39cb243'
-const 
+const API_KEY = '804511dd3d7ef6d6dcdd03d0d39cb243';
+const URL = 'https://api.themoviedb.org/3';
+const SEARCH = '/search/movie?';
+const QUERY = 'query=';
+const ADULT = '&include_adult=false;
+const LANGUAGE = '&language=';
+const PAGE = '&page=';
